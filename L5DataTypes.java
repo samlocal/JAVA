d93a69687014f0f7863700fd4051da8960dab5a9
Sameer Basha <sameerndl28@gmail.com>
2024-12-17 19:56:37 +0530

    Java Language

---
+++ L5DataTypes.java
@@ -0,0 +1,11 @@
+public class L5DataTypes {
+  
+
+  public static void main(String[] args) {
+   //var keyword(only for local variable)
+   var v1=12;
+   var v2="Sameer";
+   System.out.println("var as int ->"+v1);
+   System.out.println("var as String ->"+v2);
+  }
+}

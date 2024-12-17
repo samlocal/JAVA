3fa98b1591228a91628d9e3169f3ca289cdfefd4
Sameer Basha <sameerndl28@gmail.com>
2024-12-17 14:30:06 +0530

    Java Language

--- L1Class.java
+++ L1Class.java
@@ -1,3 +1,14 @@
 public class L1Class {
-  
+  public static void main(String args[])
+  {
+    //Creating object (obj1) of L1Class
+    L1Class obj1=new L1Class();
+    //Printing the address of the obj1
+    System.out.println("Address of obj1 is "+obj1);
+
+    //Creating object (obj2) of L1Class
+    L1Class obj2=new L1Class();
+    //Printing the address of the obj1
+    System.out.println("Address of obj2 is "+obj2);
+  }
 }
